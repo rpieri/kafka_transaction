d10de32a3886a2f7a09163c29ee56a857c10ee83
Raphael Fulgencio <raphael.fulgencio@AC-LPT-00931.local>
2024-05-09 14:52:10 -0300

    Commit

---
+++ HeartRateZoneService/Domain/HeartRateZoneReached.cs
@@ -0,0 +1,19 @@
+﻿namespace HeartRateZoneService.Domain;
+
+public class HeartRateZoneReached
+{
+    public Guid DeviceId { get; }
+    public HeartRateZone Zone { get; }
+    public DateTime DateTime { get; }
+    public int HeartRate { get; }
+    public int MaxHeartRate { get; }
+
+    public HeartRateZoneReached(Guid deviceId, HeartRateZone zone, DateTime dateTime, int heartRate, int maxHeartRate)
+    {
+        DeviceId = deviceId;
+        Zone = zone;
+        DateTime = dateTime;
+        HeartRate = heartRate;
+        MaxHeartRate = maxHeartRate;
+    }
+}
